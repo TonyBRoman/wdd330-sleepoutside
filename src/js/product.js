94430d593db42a64c0bbe2922b89ed781a9dd2b1
TonyBRoman <tonybaggon@gmail.com>
2025-07-08 17:39:37 -0600

    Dynamic Product Detail Page

--- src/js/product.js
+++ src/js/product.js
@@ -1,28 +1,33 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
 
-function addProductToCart(product) {
-  let cartItems = getLocalStorage("so-cart") || [];
-  if (!cartItems) {
-    cartItems = [];
-  }
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
-  cartItems = Array.isArray(cartItems) ? cartItems : [];
-  // console.log("cartItems:", cartItems);
-  // console.log("It is an array?", Array.isArray(cartItems));
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
+// function addProductToCart(product) {
+//   let cartItems = getLocalStorage("so-cart") || [];
+//   if (!cartItems) {
+//     cartItems = [];
+//   }
 
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+//   cartItems = Array.isArray(cartItems) ? cartItems : [];
+//   // console.log("cartItems:", cartItems);
+//   // console.log("It is an array?", Array.isArray(cartItems));
+//   cartItems.push(product);
+//   setLocalStorage("so-cart", cartItems);
+// }
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
+
+// // add listener to Add to Cart button
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
